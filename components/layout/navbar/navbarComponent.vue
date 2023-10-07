6bdaedeb37a6b667b778791e808c5f4255bedd9d
Danny <dannyvdweijde@gmail.com>
2023-10-07 18:07:24 +0200

    Started working on a navbar.

---
+++ components/layout/navbar/navbarComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <navbar-item-component
+      v-for="(navbarItem, index) in navbarItems"
+      :index="index"
+      :item="navbarItem"
+    ></navbar-item-component>
+  </nav>
+</template>
+
+<script setup lang="ts">
+  import NavbarItemComponent from './navbarItemComponent.vue'
+
+  defineProps({
+    navbarItems: {type: Object, default() {return {}}}
+  })
+</script>
---
+++ components/layout/navbar/navbarItemComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-link :to="{name: index}">
+    {{ item }}
+  </router-link>
+</template>
+
+<script setup lang="ts">
+  defineProps({
+    index: {type: String, default: null },
+    item: {type: String, default: null },
+  })
+</script>
