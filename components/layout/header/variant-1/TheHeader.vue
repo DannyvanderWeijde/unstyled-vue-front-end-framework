ba19bc8eefe074a9f27b1f901564d0fa98331724
Danny <dannyvdweijde@gmail.com>
2024-01-25 20:38:54 +0100

    Continued creating the navbar.

--- components/layout/header/variant-1/TheHeader.vue
+++ components/layout/header/variant-1/TheHeader.vue
@@ -1,14 +1,14 @@
 <template>
   <header :class="['framework-app__header', {'framework-app__header--sticky': sticky}, blockClass]">
-    <div v-if="$slots.left" class="framework-app__header-item">
+    <div v-if="$slots.left" class="framework-app__header-item framework-app__header-item--left">
       <slot name="left"></slot>
     </div>
 
-    <div v-if="$slots.middle" class="framework-app__header-item">
+    <div v-if="$slots.middle" class="framework-app__header-item framework-app__header-item--middle">
       <slot name="middle"></slot>
     </div>
 
-    <div v-if="$slots.right" class="framework-app__header-item">
+    <div v-if="$slots.right" class="framework-app__header-item framework-app__header-item--right">
       <slot name="right"></slot>
     </div>
   </header>
